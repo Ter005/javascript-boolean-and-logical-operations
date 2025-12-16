b93991d7e051f0ffa85a200ddb2931f6a83442a4
Chaiyan <chaiyan.p@hotmail.com>
2025-12-16 23:27:56 +0700

    func:boolean

--- ex-2.js
+++ ex-2.js
@@ -6,7 +6,34 @@ let isWeekday;
 let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
-
 let hasPromotion;
 
+lastMonthPaidMoreThan4000 = 4000
+isWeekday = 'Monday'||'Tuesday'||'Wenesday'||'Thursday'||'Friday'
+hasBoughtProductFromITCategory = 'IT'
+hasAttendedDiscountEvent > 0
+isPlatinum = 'Platinum'
+
+
+// case 1
+hasPromotion = (lastMonthPaidMoreThan4000 > 4000)&&(isWeekday=true)&&!hasBoughtProductFromITCategory&& hasAttendedDiscountEvent==0
+console.log(hasPromotion);
+// casd 2
+hasPromotion = isPlatinum =='Platinum'
+console.log(hasPromotion);
+
+// John
+hasPromotion = lastMonthPaidMoreThan4000 == 4001&& isWeekday == 'Friday' && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent ==2 && isPlatinum =='Gold'
+console.log(hasPromotion);
+
+
+// Reassign as John
+lastMonthPaidMoreThan4000 =4001
+isWeekday = 'Friday'
+hasBoughtProductFromITCategory = 'IT'
+hasAttendedDiscountEvent = 2
+isPlatinum = 'Gold'
+
+// John after reassign
+hasPromotion = lastMonthPaidMoreThan4000 == 4001&& isWeekday == 'Friday' && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent ==2 && isPlatinum =='Gold'
 console.log(hasPromotion);
